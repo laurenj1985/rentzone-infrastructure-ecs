ec7d062bd8eed64aeae853e7d6e81243731603d7
Lauren Johnson <lchristopher.johnson@gmail.com>
2023-09-23 11:04:37 -0400

    update referenced modules

---
+++ main.tf
@@ -0,0 +1,61 @@
+locals {
+  region       = var.region
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+# create vpc module
+module "vpc" {
+  source                       = "git@github.com:laurenj1985/App-Deployment-AWS.git//vpc"
+  region                       = local.region
+  project_name                 = local.project_name
+  environment                  = local.environment
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
+
+# create nat-gateway
+module "nat_gateway" {
+  source                     = "git@github.com:laurenj1985/App-Deployment-AWS.git//nat-gateway"
+  project_name               = local.project_name
+  environment                = local.environment
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  internet_gateway           = module.vpc.internet_gateway
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+}
+
+# create security groups
+module "security_group" {
+  source       = "git@github.com:laurenj1985/App-Deployment-AWS.git//security-groups"
+  project_name = local.project_name
+  environment  = local.environment
+  vpc_id       = module.vpc.vpc_id
+  ssh_ip       = var.ssh_ip
+}
+
+# launch rds instance
+module "rds" {
+  source                     = "git@github.com:laurenj1985/App-Deployment-AWS.git//rds"
+  project_name               = local.project_name
+  environment                = local.environment
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+  database_security_group_id = module.security_group.database_security_group_id
+}
+
+# request ssl certificate
+module "ssl_certificate" {
+  source            = "git@github.com:laurenj1985/App-Deployment-AWS.git//acm"
+  domain_name       = var.domain_name
+  alternative_names = var.alternative_names
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+#environment variables
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+
+# vpc variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
+
+# security groups variables
+variable "ssh_ip" {}
+
+# rds variables
+
+# acm variables
+variable "domain_name" {}
+variable "alternative_names" {}
